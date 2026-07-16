fbbe5eee6070a102fdf3665b9c1dae4cf5ef0d49
Your Name <you@example.com>
2026-07-16 14:25:26 +0000

    -

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 gcc -Icore -Itransport -Ihttp -o serv serv.c core/*c transport/*.c http/*.c && ./serv
 gcc -g -O0 -Icore -Itransport -Ihttp -o serv serv.c core/*c transport/*.c http/*.c && gdb ./ser
+printf "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\nName: Nhi\r\n\r\n" | nc localhost 8080
