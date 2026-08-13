55d8ca2524d789fc2abfb3770ee8f2b2698e5e08
Your Name <you@example.com>
2026-08-13 18:29:51 +0000

    -

---
+++ event/ngd_epoll.h
@@ -0,0 +1,26 @@
+#ifndef NGD_EPOLL_H
+#define NGD_EPOLL_H
+//
+#define NGD_EPOLL_READ
+#define NGD_EPOLL_WRITE
+#define NGD_EPOLL_HUP
+#define NGD_EPOLL_MAXEVENTS 1024
+//
+static int epfd;
+static struct epoll_event[NGD_EPOLL_MAXEVENTS]
+//
+int ngd_epoll_regis(fd, interest_flag);
+int ngd_epoll_unregis(...);
+int ngd_epoll_loop(...);
+
+
+int process_events_and_timers(events...)
+{
+    for (;;)
+    {
+
+        int n = process_events()
+    }
+}
+//
+#end
