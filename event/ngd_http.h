5b6654e87e13552be80741320c12dd0651733bc7
Your Name <you@example.com>
2026-08-15 07:11:40 +0000

    -

--- event/ngd_http.h
+++ event/ngd_http.h
@@ -2,6 +2,7 @@
 #define NGD_HTTP_CONN_H
 //
 typedef struct ngd_http_t ngd_http_t;
+typedef struct ngd_req_t ngd_req_t;
 //
 struct ngd_http_t {
     ngd_req_t *r;
@@ -11,6 +12,30 @@ struct ngd_http_t {
     ngd_buf_t *out;
     //
 };
+
+struct ngd_parser_reqline {
+    u_char *start_method;
+    u_char *end_method;
+    u_char *start_uri;
+    u_char *end_uri;
+    u_char *start_ver;
+    u_char *end_ver;
+}
+
+struct ngd_parser_headerline {
+    u_char *key_start;
+    u_char *key_end;
+    u_char *value_start;
+    u_char *value_end;
+}
+//
+struct ngd_req_t {
+    ngd_str_t *smethod, *suri, *sver;
+    buf_t *in;
+    size_t content_length;
+    //
+}
+//
 
 ngd_http_handle(ngd_conn_t *c)
 {
@@ -26,12 +51,20 @@ ngd_http_handle_read(ngd_http_t *http)
     enum {
         proc_reqline,
         proc_headers,
-        proc_body
+        proc_body,
     } state;
-    switch (http->state)
+    //
+    state = http->state;
+    //
+    switch ()
     {
-
+        case proc_reqline: break;
+        case proc_headers: break;
+        case proc_body: break;
     }
+    http->state;
+    //
+
 }
 
 ngd_http_handle_write(ngd_http_t *http)
@@ -42,13 +75,19 @@ ngd_http_handle_write(ngd_http_t *http)
         proc_send_response
     } state;
     //
-    state = ..;
-
+    state = http->state;
+    //
     switch (state)
     {
-        ...
+        case proc_build_response: break;
+        case proc_compress_response: break;
+        case proc_send_response:break;
+        //
+        case proc_ : break;
+
     }
     .. = state;
 
 }
+
 #endif
