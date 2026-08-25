34e0060247fbb561a0376d1aaf2b6ac4dca6261c
Your Name <you@example.com>
2026-08-25 06:00:27 +0000

    -

--- event_test/http/ngd_str.h
+++ event_test/http/ngd_str.h
@@ -6,6 +6,16 @@
 #define NGD_STR(s) \
     (ngd_str_t){ .data = (s), .len = sizeof(s) - 1 }
 //
-
+typedef struct ngd_str_t ngd_str_t;
+//
+struct ngd_str_t {
+    void *data;
+    size_t len;
+};
+//
+int str_cmp(str_t *s1, str_t *s2);
+void str_zeros(void *data, size_t len);
+ngd_str_print(ngd_str_t *s);
+//
 //
 #endif
