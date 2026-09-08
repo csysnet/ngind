65d4c14047630c984d2ad4241dfeb2f675df3bf0
Your Name <you@example.com>
2026-09-08 07:19:37 +0000

    -

--- script.sh
+++ script.sh
@@ -1,8 +1,12 @@
 gcc -g -O0 -Icore -Itransport -Ihttp -o server server.c core/*c transport/*.c http/*.c && gdb ./ser
 clear && gcc -Icore -Itransport -Ihttp -o serv serv.c core/*c transport/*.c http/*.c && ./serv
-printf "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\nContent-Length: 123\r\n\r\n" | nc localhost 8080
+printf "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n\r\n" | nc localhost 8080
 printf "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\nTransfer-Encoding: chunked\r\n\r\n" | nc localhost 8080
 //
 printf "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\nContent-Length: 123\r\n\r\nA\r\nphamtandat\r\n0\r\n\r\n" | nc localhost 8080
 //
-clear && gcc -Icore -Itransport -Ihttp -o serv server.c core/*c transport/*.c http/*.c
+clear && gcc -Icore -Itransport -Ihttp -o server server.c core/*c transport/*.c http/*.c && ./server
+//
+ngd_str_log("reach here");
+//
+printf "GET /index.html HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n\r\n" | nc localhost 8080
