13e425668c1bf8482367697d35ef2c948584018c
Your Name <you@example.com>
2026-09-09 18:29:54 +0000

    -

--- src/http/ngd_http.c
+++ src/http/ngd_http.c
@@ -226,6 +226,14 @@ ngd_http_handle_conn(ngd_conn_t *c)
                 if (ret == NGD_AGAIN)
                     goto again;
                 if (ret == NGD_HTTP_FULL_SEND_DONE) {
+                    ngd_str_print(NGD_STR_C("Success: \n");
+                    ngd_str_print(http->smethod);
+                    ngd_str_print(NGD_STR_C(" "));
+                    ngd_str_print(http->suri);
+                    ngd_str_print(NGD_STR_C(" "));
+                    ngd_str_print(http->sver);
+                    ngd_str_print(NGD_STR_C("\n"));
+                    //
                     ngd_file_close(&http->file_send);
                     if (!http->on_keep_alive) {
                         ngd_conn_disable_write(c);
